481ecc926cd8bc719d52d7d14b7110ce0b4686f4
Dj <ritchey0713@gmail.com>
2020-03-11 21:12:21 -0400

    wire toggle for success message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'type-game';
+  sentence = lorem.sentence()
+  successMessageHidden = true
+
+  getSentence = () => (this.sentence)
+
+  onChangeInput = (value: string) => {
+    if(value === this.sentence){
+      this.successMessageHidden = false
+    }
+  }
+
+
 }
